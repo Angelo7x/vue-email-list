0a3ca59fb892e6e3c1d6596ec3376f28bdc91621
Angelo7x <abennici96@gmail.com>
2021-09-27 17:41:45 +0200

    Creata lista

--- js/main.js
+++ js/main.js
@@ -11,9 +11,10 @@ const app = new Vue({
     mounted() {
         for ( var i = 0; i < 10 ; i ++ ) {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((response)) => {
+            .then((response) => {
                 this.emails.push(response.data.response);
             });
+            
         }
 
         },
